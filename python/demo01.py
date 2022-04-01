7397a2b2fa7295334b1eda62d818d2fed38e0d9f
Aubrey Han <aubreyhan@live.com>
2022-04-01 06:58:10 +0000

    dsa

--- python/demo01.py
+++ python/demo01.py
@@ -1,1 +1,15 @@
-from google.com import speech
+from google.cloud import speech
+
+client = speech.SpeechClient()
+
+audio = speech.RecognitionAudio(uri='gs://hydemo01/demo01.flac')
+
+config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=16000,
+        language_code="zh-CN",
+    )
+
+response = client.recognize(config=config, audio=audio)
+
+#print (client)
