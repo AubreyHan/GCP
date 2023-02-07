803c0fef87de8eec5a854aee1a48301b5eef8e25
Aubrey Han <aubreyhan@live.com>
2023-02-07 03:16:02 +0000

    policy

---
+++ Argolis/policy/policy.sh
@@ -0,0 +1,5 @@
+gcloud org-policies set-policy os_login.yaml
+gcloud org-policies set-policy restrictVpcPeering.yaml
+gcloud org-policies set-policy shieldedVm.yaml
+gcloud org-policies set-policy vmCanIpForward.yaml
+gcloud org-policies set-policy vmExternalIpAccess.yaml
