2976ca8fec14445b6949f88e12489f0e4b66afbb
Aubrey Han <aubreyhan@live.com>
2023-02-16 01:11:21 +0000

    mig

--- Argolis/temp/mig.sh
+++ Argolis/temp/mig.sh
@@ -1,5 +1,5 @@
 #Env
-ProjectID="hy-20230207-001"
+ProjectID="hy-20230214-001"
 VMName="vmdemo"
 Disk="vmdemo"
 Image="vmdemo-image"
@@ -9,11 +9,11 @@ InstanceTemplate="vm-instance-template"
 MachineType="n2-standard-2"
 
 #Stop Template VM
-#gcloud compute instances stop --project=$ProjectID --zone=$Zone $VMName
+gcloud compute instances stop --project=$ProjectID --zone=$Zone $VMName-v2
 
 #Capture Disk Image
-#gcloud compute images create $Image-v1 --project=$ProjectID --source-disk=$Disk  --source-disk-zone=$Zone --storage-location=$Region
+gcloud compute images create $Image-v2 --project=$ProjectID --source-disk=$Disk-v2  --source-disk-zone=$Zone --storage-location=$Region
 
 #Create Instance Template
-#gcloud compute instance-templates create $InstanceTemplate-v1 --project=$ProjectID --machine-type=$MachineType --network-interface=network=default,network-tier=PREMIUM --tags=http-server,https-server --create-disk=auto-delete=yes,boot=yes,image=projects/$ProjectID/global/images/$Image-v1,size=10,type=pd-balanced
+gcloud compute instance-templates create $InstanceTemplate-v2 --project=$ProjectID --machine-type=$MachineType --network-interface=network=default,network-tier=PREMIUM --tags=http-server,https-server --create-disk=auto-delete=yes,boot=yes,image=projects/$ProjectID/global/images/$Image-v2,size=10,type=pd-balanced
 
