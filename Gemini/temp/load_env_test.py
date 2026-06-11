2c3044636e31486e56d2c782e42abd3c419bc194
Aubrey Han <aubreyhan@live.com>
2026-06-12 06:18:51 +0800

    Auto-sync: local changes (2026-06-12 06:18:51)

--- Gemini/temp/load_env_test.py
+++ Gemini/temp/load_env_test.py
@@ -1,7 +1,9 @@
+from pickle import TRUE
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv(override=True)
 
-if tr
-print(os.environ.get("PROJECT_ID"))
+if TRUE:
+    print(os.environ.get("PROJECT_ID"))
