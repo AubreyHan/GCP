699f5cc81bcb212ff08762479f564d29f8eed2cc
Aubrey Han <aubreyhan@live.com>
2026-06-15 19:24:59 +0800

    Auto-sync: local changes 2026-06-15 19:24:59

--- Gemini/RestAPI/run.sh
+++ Gemini/RestAPI/run.sh
@@ -2,4 +2,4 @@ curl -X POST \
   -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
   -H "Content-Type: application/json; charset=utf-8" \
   -d @request.json \
-  "https://us-central1-aiplatform.googleapis.com/v1/projects/hy-ai-demo/locations/us-central1/publishers/google/models/gemini-2.5-flash-001:generateContent"
+  "https://aiplatform.googleapis.com/v1/projects/hy-ai-demo/locations/us-central1/publishers/google/models/gemini-2.5-flash-001:generateContent"
