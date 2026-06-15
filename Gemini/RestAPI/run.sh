082dc9e8ac0e4dc7620ddb42457f33b4c17702ff
Aubrey Han <aubreyhan@live.com>
2026-06-15 19:21:29 +0800

    Auto-sync: local changes 2026-06-15 19:21:29

--- Gemini/RestAPI/run.sh
+++ Gemini/RestAPI/run.sh
@@ -1,1 +1,5 @@
-curl 
+curl -X POST \
+  -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -d @request.json \
+  "https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/gemini-2.5-flash-001:generateContent"
