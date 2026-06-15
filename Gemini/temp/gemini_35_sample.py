66dd9b5fdbd474268c92cda2e6e92c850aea6d20
Aubrey han <aubreyhan@live.com>
2026-06-15 11:37:26 +0800

    Auto-sync: local changes 2026-06-15 11:37:26

--- Gemini/temp/gemini_35_sample.py
+++ Gemini/temp/gemini_35_sample.py
@@ -22,10 +22,10 @@ def main():
     http_opts = types.HttpOptions(base_url=base_url) if base_url else None
 
     client = genai.Client(
-        api_key=api_key,
-        #project=project_id,
-        #location=location,
-        #http_options=http_opts,
+        #api_key=api_key,
+        project=project_id,
+        location=location,
+        http_options=http_opts,
         vertexai=True,
     )
     
