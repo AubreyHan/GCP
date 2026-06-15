d62e6fc30bcaf047b14b27cd86e73cdba364f2c9
Aubrey Han <aubreyhan@live.com>
2026-06-15 17:27:56 +0800

    Auto-sync: local changes 2026-06-15 17:27:56

--- Gemini/RestAPI/run.sh
+++ Gemini/RestAPI/run.sh
@@ -38,4 +38,4 @@ curl -X POST \
         "maxOutputTokens": 1024
     }
 }' \
-  "https://aiplatform.googleapis.com/v1/projects/hy-ai-demo/locations/global/publishers/google/models/gemini-2.5-flash-001:generateContent"
+  “https://us-central1-aiplatform.googleapis.com/v1/projects/hy-ai-demo/locations/us-central1/publishers/google/models/gemini-2.5-flash-001:generateContent”
