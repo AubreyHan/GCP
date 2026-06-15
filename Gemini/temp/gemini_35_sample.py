78e3167b4706fe1eee62ddb0fe74c5ddd5e896ec
Aubrey han <aubreyhan@live.com>
2026-06-15 11:56:26 +0800

    Auto-sync: local changes 2026-06-15 11:56:26

--- Gemini/temp/gemini_35_sample.py
+++ Gemini/temp/gemini_35_sample.py
@@ -10,8 +10,8 @@ api_key = os.environ.get("API_KEY")
 def generate():
   client = genai.Client(
       vertexai=True,
-      project="hy-ai-demo",
-      location="global",
+      #project="hy-ai-demo",
+      #location="global",
       api_key=api_key,
   )
 
