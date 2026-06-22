a81a0ca8d84651007da17f34e12fc503802307ca
Aubrey han <aubreyhan@live.com>
2026-06-22 08:23:49 +0800

    Auto-sync: local changes 2026-06-22 08:23:49

--- Gemini/sample_code/gemini_batch_job_gcs.py
+++ Gemini/sample_code/gemini_batch_job_gcs.py
@@ -5,5 +5,7 @@ from google.genai.types import CreateBatchJobConfig, JobState, HttpOptions
 from Gemini.loopit.loopit import project_id
 
 client = genai.Client(
-  
+  enterprise=True,
+  project='hy-ai-demo',
+  location=
 )
