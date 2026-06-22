a6083e82e748f65b0f69e6b8797032676b1e123c
Aubrey han <aubreyhan@live.com>
2026-06-22 08:22:49 +0800

    Auto-sync: local changes 2026-06-22 08:22:49

--- Gemini/sample_code/gemini_batch_job_gcs.py
+++ Gemini/sample_code/gemini_batch_job_gcs.py
@@ -2,6 +2,8 @@ import time
 from google import genai
 from google.genai.types import CreateBatchJobConfig, JobState, HttpOptions
 
+from Gemini.loopit.loopit import project_id
+
 client = genai.client(
-  vertex
+
 )
