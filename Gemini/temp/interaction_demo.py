07770ec3c5fa30a73b59cd3efde396ec5b88eee7
Aubrey han <aubreyhan@live.com>
2026-06-25 09:40:21 +0800

    Auto-sync: local changes 2026-06-25 09:40:21

--- Gemini/temp/interaction_demo.py
+++ Gemini/temp/interaction_demo.py
@@ -1,3 +1,10 @@
 from google import genai
 from google.genai import types
 from google.genai.types import HttpOptions
+
+from dotenv import load_dotenv
+
+if load_dotenv(override=True):
+  pass
+
+client = gen
