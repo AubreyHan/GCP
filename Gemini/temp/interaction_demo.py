152f6146bad2d54f1c1ab3b202bd7ac1a669ae1a
Aubrey han <aubreyhan@live.com>
2026-06-25 11:39:46 +0800

    Auto-sync: local changes 2026-06-25 11:39:46

--- Gemini/temp/interaction_demo.py
+++ Gemini/temp/interaction_demo.py
@@ -21,44 +21,8 @@ client = genai.Client(
 )
 
 model = "gemini-3.5-flash"
-
-contents = [
-  types.Content(
-    role="user",
-    parts=[
-      types.Part.from_text(text="""hello""")
-    ]
-  )
-]
-
-generate_content_config = types.GenerateContentConfig(
-  temperature = 1,
-  top_p = 0.95,
-  max_output_tokens = 65535,
-  safety_settings = [types.SafetySetting(
-    category="HARM_CATEGORY_HATE_SPEECH",
-    threshold="OFF"
-  ),types.SafetySetting(
-    category="HARM_CATEGORY_DANGEROUS_CONTENT",
-    threshold="OFF"
-  ),types.SafetySetting(
-    category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    threshold="OFF"
-  ),types.SafetySetting(
-    category="HARM_CATEGORY_HARASSMENT",
-    threshold="OFF"
-  )],
-  thinking_config=types.ThinkingConfig(
-    thinking_level="MEDIUM",
-  ),
+interaction = client.interactions.create(
+    model='gemini-3.5-flash',
+    input='Tell me a short joke about programming.'
 )
-
-resopnse_turn01 = client.interactions.create(
-  model=model,
-  #generation_config=generate_content_config,
-  input="who are you"
-)
-
-print(resopnse_turn01)
-
-
+print(interaction.outputs[-1].text)
