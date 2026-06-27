a6e66f4d45d7620975d477e5654af36bee26d2a0
Aubrey Han <aubreyhan@live.com>
2026-06-27 21:05:09 +0800

    Auto-sync: local changes 2026-06-27 21:05:09

--- Gemini/temp/demo001.py
+++ Gemini/temp/demo001.py
@@ -19,6 +19,26 @@ model = "gemini-3.1-flash-lite"
 
 start_time = time.time()
 
-contents = "how tall is the eiffel tower?"
+contents = [
+    types.Content(
+      role="user",
+      parts=[
+        types.Part.from_text(text="""
+        逻辑推理题 - 三神
+        有代号 A, B, C 的三位神祇，
+        只知祂们名为“真实、虚谎、任性”，
+        但不知哪个代号属哪个名字。
+        真实之神只说真话，虚谎之神只说假话，
+        而任性之神会随意说真话或假话。
+        你的任务需找出 A, B, C 的身份，
+但每次只能向一位神祇发问。
+神祇们都懂得你的语言，
+但只会用祂们的语言回答 "da" 或 "ja"。
+这两种回答，一个解“是”，一个解“否”，
+但你不知道哪个回答哪个意思。
+你该如何确定每个神的身份？""")
+      ]
+    )
+  ]
 
 
