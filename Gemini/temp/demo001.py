0f64cdcc959d271521d897315bc2ca30396e785a
Aubrey Han <aubreyhan@live.com>
2026-06-27 20:57:07 +0800

    Auto-sync: local changes 2026-06-27 20:57:07

--- Gemini/temp/demo001.py
+++ Gemini/temp/demo001.py
@@ -1,2 +1,17 @@
+from http import client
+from typing import overload
+
 from google import genai
 from dotenv import load_dotenv
+import os
+import time
+
+if load_dotenv(overload=True):
+    pass
+
+api_key = os.getenv("GOOGLE_API_KEY")
+    
+
+
+client = genai.Client(
+)
