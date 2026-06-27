103e8f27d9efb62672d1761bcfd835d283f360b7
Aubrey Han <aubreyhan@live.com>
2026-06-27 21:07:01 +0800

    Auto-sync: local changes 2026-06-27 21:07:01

--- Gemini/temp/demo001.py
+++ Gemini/temp/demo001.py
@@ -42,4 +42,24 @@ contents = [
     )
   ]
 
+generate_content_config = types.GenerateContentConfig(
+  temperature = 1,
+  top_p = 0.95,
+  max_output_tokens = 65535,
+  safety_settings = [types.SafetySetting(
+    category="HARM_CATEGORY_HATE_SPEECH",
+    threshold="OFF"
+  ),types.SafetySetting(
+    category="HARM_CATEGORY_DANGEROUS_CONTENT",
+    threshold="OFF"
+  ),types.SafetySetting(
+    category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    threshold="OFF"
+  ),types.SafetySetting(
+    category="HARM_CATEGORY_HARASSMENT",
+    threshold="OFF"
+  )],
+)
+
+respnse = 
 
