3dc8f6f08b3d15aafa3c3a78b9bbd1ca9552cd03
Aubrey han <aubreyhan@live.com>
2026-07-01 09:45:31 +0800

    Auto-sync: local changes 2026-07-01 09:45:31

--- Gemini/Temp/NBP.py
+++ Gemini/Temp/NBP.py
@@ -1,3 +1,9 @@
 from google import genai
 from google.genai import types
-from 
+from dotenv import load_dotenv
+import os
+
+load_dotenv  # pyright: ignore[reportUnusedExpression]
+
+api_key = os.getenv("API_KEY")
+print(api_key)
